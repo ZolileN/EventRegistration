fa8585cb10355dc3b47c755db06992f81991f2d7
Kevin <kelaframboise@gmail.com>
2016-09-13 00:59:53 -0400

    compiled umple code

---
+++ src/ca/mcgill/ecse321/eventregistration/model/Event.java
@@ -0,0 +1,104 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.20.1.4071 modeling language!*/
+
+package ca.mcgill.ecse321.eventregistration.model;
+import java.sql.Date;
+import java.sql.Time;
+
+// line 7 "../../../../../EventRegistration.ump"
+public class Event
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //Event Attributes
+  private String name;
+  private Date date;
+  private Time startTime;
+  private Time endTime;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public Event(String aName, Date aDate, Time aStartTime, Time aEndTime)
+  {
+    name = aName;
+    date = aDate;
+    startTime = aStartTime;
+    endTime = aEndTime;
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setName(String aName)
+  {
+    boolean wasSet = false;
+    name = aName;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setDate(Date aDate)
+  {
+    boolean wasSet = false;
+    date = aDate;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setStartTime(Time aStartTime)
+  {
+    boolean wasSet = false;
+    startTime = aStartTime;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setEndTime(Time aEndTime)
+  {
+    boolean wasSet = false;
+    endTime = aEndTime;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public Date getDate()
+  {
+    return date;
+  }
+
+  public Time getStartTime()
+  {
+    return startTime;
+  }
+
+  public Time getEndTime()
+  {
+    return endTime;
+  }
+
+  public void delete()
+  {}
+
+
+  public String toString()
+  {
+	  String outputString = "";
+    return super.toString() + "["+
+            "name" + ":" + getName()+ "]" + System.getProperties().getProperty("line.separator") +
+            "  " + "date" + "=" + (getDate() != null ? !getDate().equals(this)  ? getDate().toString().replaceAll("  ","    ") : "this" : "null") + System.getProperties().getProperty("line.separator") +
+            "  " + "startTime" + "=" + (getStartTime() != null ? !getStartTime().equals(this)  ? getStartTime().toString().replaceAll("  ","    ") : "this" : "null") + System.getProperties().getProperty("line.separator") +
+            "  " + "endTime" + "=" + (getEndTime() != null ? !getEndTime().equals(this)  ? getEndTime().toString().replaceAll("  ","    ") : "this" : "null")
+     + outputString;
+  }
+}
